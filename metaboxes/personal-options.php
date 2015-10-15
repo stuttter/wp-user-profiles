a59bdccdde192b5f69f75747dd22a284d7c7f182
John James Jacoby <johnjamesjacoby@me.com>
2015-10-15 07:50:35 -0500

    Always allow users to edit all personal options.
    User preferences should not be limited to capabilities.

--- metaboxes/personal-options.php
+++ metaboxes/personal-options.php
@@ -2,7 +2,7 @@
 
 /**
  * User Profile Personal-Options Metabox
- * 
+ *
  * @package User/Profiles/Metaboxes/PersonalOptions
  */
 
@@ -21,26 +21,23 @@ function wp_user_profiles_personal_options_metabox( $user = null ) {
 
 	<table class="form-table">
 
-		<?php if ( ! ( IS_PROFILE_PAGE && ! ( current_user_can( 'edit_posts' ) || current_user_can( 'edit_pages' ) ) ) ) : ?>
+		<tr class="user-rich-editing-wrap">
+			<th scope="row"><?php esc_html_e( 'Visual Editor', 'wp-user-profiles' ); ?></th>
+			<td>
+				<label for="rich_editing"><input name="rich_editing" type="checkbox" id="rich_editing" value="false" <?php checked( 'false', $user->rich_editing ); ?> />
+					<?php esc_html_e( 'Disable the visual editor when writing', 'wp-user-profiles' ); ?>
+				</label>
+			</td>
+		</tr>
 
-			<tr class="user-rich-editing-wrap">
-				<th scope="row"><?php esc_html_e( 'Visual Editor', 'wp-user-profiles' ); ?></th>
-				<td>
-					<label for="rich_editing"><input name="rich_editing" type="checkbox" id="rich_editing" value="false" <?php checked( 'false', $user->rich_editing ); ?> />
-						<?php esc_html_e( 'Disable the visual editor when writing', 'wp-user-profiles' ); ?>
-					</label>
-				</td>
-			</tr>
-			<tr class="user-comment-shortcuts-wrap">
-				<th scope="row"><?php esc_html_e( 'Keyboard Shortcuts', 'wp-user-profiles' ); ?></th>
-				<td>
-					<label for="comment_shortcuts"><input type="checkbox" name="comment_shortcuts" id="comment_shortcuts" value="true" <?php checked( 'true', $user->comment_shortcuts ); ?> />
-						<?php esc_html_e( 'Enable keyboard shortcuts for comment moderation.', 'wp-user-profiles' ); ?>
-					</label>
-				</td>
-			</tr>
-
-		<?php endif; ?>
+		<tr class="user-comment-shortcuts-wrap">
+			<th scope="row"><?php esc_html_e( 'Keyboard Shortcuts', 'wp-user-profiles' ); ?></th>
+			<td>
+				<label for="comment_shortcuts"><input type="checkbox" name="comment_shortcuts" id="comment_shortcuts" value="true" <?php checked( 'true', $user->comment_shortcuts ); ?> />
+					<?php esc_html_e( 'Enable keyboard shortcuts for comment moderation.', 'wp-user-profiles' ); ?>
+				</label>
+			</td>
+		</tr>
 
 		<tr class="show-admin-bar user-admin-bar-front-wrap">
 			<th scope="row"><?php esc_html_e( 'Toolbar', 'wp-user-profiles' ); ?></th>
@@ -54,6 +51,7 @@ function wp_user_profiles_personal_options_metabox( $user = null ) {
 				</fieldset>
 			</td>
 		</tr>
+
 		<?php
 		/**
 		 * Fires at the end of the 'Personal Options' settings table on the user editing screen.
