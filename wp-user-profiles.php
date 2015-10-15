f5c29f953cb0096b9604f0a4f5ee012cf1864c87
John James Jacoby <johnjamesjacoby@me.com>
2015-10-15 10:26:04 -0500

    Update description.

--- wp-user-profiles.php
+++ wp-user-profiles.php
@@ -5,7 +5,7 @@
  * Plugin URI:  https://wordpress.org/plugins/wp-user-profiles/
  * Description: A sophisticated way to edit users in WordPress
  * Author:      John James Jacoby
- * Version:     0.1.1
+ * Version:     0.1.2
  * Author URI:  https://profiles.wordpress.org/johnjamesjacoby/
  * License:     GPL v2 or later
  */
