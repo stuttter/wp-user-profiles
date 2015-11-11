23481bf0fd42c04420d9941298c0112ebccc3b00
John James Jacoby <johnjamesjacoby@me.com>
2015-11-11 11:24:49 -0600

    Remove removed file.

--- wp-user-profiles.php
+++ wp-user-profiles.php
@@ -28,7 +28,6 @@ function _wp_user_profiles() {
 
 	// Metaboxes
 	require_once $plugin_path . 'metaboxes/about.php';
-	require_once $plugin_path . 'metaboxes/capabilities.php';
 	require_once $plugin_path . 'metaboxes/color-scheme.php';
 	require_once $plugin_path . 'metaboxes/contact.php';
 	require_once $plugin_path . 'metaboxes/email.php';
