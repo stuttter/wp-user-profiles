f573d234b0f6552a7a74c3b7adebf9edab8bc9e0
John James Jacoby <johnjamesjacoby@me.com>
2015-12-21 00:43:43 -0600

    Reorient profile hooks.

--- includes/hooks.php
+++ includes/hooks.php
@@ -17,11 +17,14 @@ add_action( 'user_admin_menu',    'wp_user_profiles_admin_menus' );
 add_action( 'admin_enqueue_scripts', 'wp_user_profiles_admin_enqueue_scripts' );
 
 // Metaboxes
-add_action( 'add_meta_boxes', 'wp_user_profiles_add_status_metabox',         10, 2 );
-add_action( 'add_meta_boxes', 'wp_user_profiles_add_profile_meta_boxes',     10, 2 );
-add_action( 'add_meta_boxes', 'wp_user_profiles_add_account_meta_boxes',     10, 2 );
-add_action( 'add_meta_boxes', 'wp_user_profiles_add_options_meta_boxes',     10, 2 );
-add_action( 'add_meta_boxes', 'wp_user_profiles_add_permissions_meta_boxes', 10, 2 );
+add_action( 'add_meta_boxes', 'wp_user_profiles_add_meta_boxes',      10, 2 );
+
+// Sections
+add_action( 'wp_user_profiles_add_meta_boxes',             'wp_user_profiles_add_status_metabox',         10, 2 );
+add_action( 'wp_user_profiles_add_profile_meta_boxes',     'wp_user_profiles_add_profile_meta_boxes',     10, 2 );
+add_action( 'wp_user_profiles_add_account_meta_boxes',     'wp_user_profiles_add_account_meta_boxes',     10, 2 );
+add_action( 'wp_user_profiles_add_options_meta_boxes',     'wp_user_profiles_add_options_meta_boxes',     10, 2 );
+add_action( 'wp_user_profiles_add_permissions_meta_boxes', 'wp_user_profiles_add_permissions_meta_boxes', 10, 2 );
 
 // Profiles
 add_filter( 'edit_profile_url',   'wp_user_profiles_edit_user_url_filter', 10, 3 );
