aad859789baee8770f6d275ee2b8e9e094d45f25
John James Jacoby <johnjamesjacoby@me.com>
2015-12-21 20:43:03 -0600

    Save "Profile" section data.

--- includes/sections/profile.php
+++ includes/sections/profile.php
@@ -64,4 +64,60 @@ class WP_User_Profile_Profile_Section extends WP_User_Profile_Section {
 			);
 		}
 	}
+
+	/**
+	 * Save section data
+	 *
+	 * @since 0.2.0
+	 *
+	 * @param WP_User $user
+	 */
+	public function save( $user = null ) {
+
+		// Setup the user login
+		if ( isset( $_POST['user_login'] ) ) {
+			$user->user_login = sanitize_user( $_POST['user_login'], true );
+		}
+
+		// First
+		$user->first_name = isset( $_POST['first_name'] )
+			? sanitize_text_field( $_POST['first_name'] )
+			: '';
+
+		// Last
+		$user->last_name = isset( $_POST['last_name'] )
+			? sanitize_text_field( $_POST['last_name'] )
+			: '';
+
+		// Nick
+		$user->nickname = isset( $_POST['nickname'] )
+			? sanitize_text_field( $_POST['nickname'] )
+			: '';
+
+		// Display
+		$user->display_name = isset( $_POST['display_name'] )
+			? sanitize_text_field( $_POST['display_name'] )
+			: '';
+
+		// Description
+		$user->description = isset( $_POST['description'] )
+			? trim( $_POST['description'] )
+			: '';
+
+		// Website
+		if ( isset( $_POST['url'] ) ) {
+			if ( empty ( $_POST['url'] ) || in_array( $_POST['url'], wp_allowed_protocols(), true ) ) {
+				$user->user_url = '';
+			} else {
+				$user->user_url = esc_url_raw( $_POST['url'] );
+				$protocols      = implode( '|', array_map( 'preg_quote', wp_allowed_protocols() ) );
+				$user->user_url = preg_match( '/^(' . $protocols . '):/is', $user->user_url )
+					? $user->user_url
+					: 'http://' . $user->user_url;
+			}
+		}
+
+		// Allow third party plugins to save data in this section
+		parent::save( $user );
+	}
 }
