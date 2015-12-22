1d47254bd53dd73946a5ee5dc29ffd5b6b6dcf97
John James Jacoby <johnjamesjacoby@me.com>
2015-12-21 21:46:28 -0600

    First pass "Account" section save.

--- includes/sections/account.php
+++ includes/sections/account.php
@@ -62,4 +62,77 @@ class WP_User_Profile_Account_Section extends WP_User_Profile_Section {
 			$user
 		);
 	}
+
+	/**
+	 * Save section data
+	 *
+	 * @since 0.2.0
+	 *
+	 * @param WP_User $user
+	 */
+	public function save( $user = null ) {
+
+		// Password changes
+		$pass1 = isset( $_POST['pass1'] )
+			? $_POST['pass1']
+			: '';
+
+		$pass2 = isset( $_POST['pass2'] )
+			? $_POST['pass2']
+			: '';
+
+		/**
+		 * Fires before the password and confirm password fields are checked for congruity.
+		 *
+		 * @since 1.5.1
+		 *
+		 * @param string $user_login The username.
+		 * @param string &$pass1     The password, passed by reference.
+		 * @param string &$pass2     The confirmed password, passed by reference.
+		 */
+		do_action_ref_array( 'check_passwords', array( $user->user_login, &$pass1, &$pass2 ) );
+
+		// Check for "\" in password
+		if ( false !== strpos( wp_unslash( $pass1 ), "\\" ) ) {
+			$this->errors->add( 'pass', __( '<strong>ERROR</strong>: Passwords may not contain the character "\\".' ), array( 'form-field' => 'pass1' ) );
+		}
+
+		// Checking the password has been typed twice the same
+		if ( $pass1 !== $pass2 ) {
+			$this->errors->add( 'pass', __( '<strong>ERROR</strong>: Please enter the same password in both password fields.' ), array( 'form-field' => 'pass1' ) );
+		}
+
+		// Set the password
+		if ( ! empty( $pass1 ) ) {
+			$user->user_pass = $pass1;
+		}
+
+		// Checking email address
+		if ( isset( $_POST['email'] ) ) {
+
+			// Sanitize the email
+			$user->user_email = sanitize_text_field( wp_unslash( $_POST['email'] ) );
+
+			// Email empty
+			if ( empty( $user->user_email ) ) {
+				$this->errors->add( 'empty_email', __( '<strong>ERROR</strong>: Please enter an email address.' ), array( 'form-field' => 'email' ) );
+
+			// Email invalid
+			} elseif ( ! is_email( $user->user_email ) ) {
+				$this->errors->add( 'invalid_email', __( '<strong>ERROR</strong>: The email address is not correct.' ), array( 'form-field' => 'email' ) );
+
+			// Email in use
+			} elseif ( ( $owner_id = email_exists( $user->user_email ) ) && ( $owner_id !== $user->ID ) ) {
+				$this->errors->add( 'email_exists', __( '<strong>ERROR</strong>: This email is already in use.' ), array( 'form-field' => 'email' ) );
+			}
+		}
+
+		// Bail if password change errors occurred
+		if ( $this->errors->get_error_code() ) {
+			return $this->errors;
+		}
+
+		// Allow third party plugins to save data in this section
+		parent::save( $user );
+	}
 }
