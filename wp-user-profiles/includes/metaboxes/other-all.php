73e1d23892082b7fc6e502a6956a218988b532ab
John James Jacoby <johnjamesjacoby@me.com>
2020-07-07 11:11:01 -0500

    Sections: Add message back into "Other > Additional" when empty.
    This only happens when plugins add hooks that are later unhooked in-between screen registration and display.

--- wp-user-profiles/includes/metaboxes/other-all.php
+++ wp-user-profiles/includes/metaboxes/other-all.php
@@ -2,7 +2,7 @@
 
 /**
  * User Profile Other Metabox
- * 
+ *
  * @package Plugins/Users/Profiles/Metaboxes/Other
  */
 
@@ -18,8 +18,26 @@ defined( 'ABSPATH' ) || exit;
  */
 function wp_user_profiles_other_metabox( $user = null ) {
 
-	// Fire legacy WordPress actions
-	wp_is_profile_page()
-		? do_action( 'show_user_profile', $user )
-		: do_action( 'edit_user_profile', $user );
+	// Start an output buffer
+	ob_start();
+
+	// Get the correct action
+	$action = wp_is_profile_page()
+		? 'show_user_profile'
+		: 'edit_user_profile';
+
+	// Fire extra WordPress actions
+	do_action( $action, $user );
+
+	// Get (and clean) the current output buffer
+	$output = ob_get_clean();
+
+	// Hooks are working
+	if ( ! empty( $output ) ) {
+		echo $output;
+
+	// Hooks are doing weird things
+	} else {
+		echo wpautop( esc_html__( 'A plugin attempted to show something here, but then failed to do so.', 'wp-user-profiles' ) );
+	}
 }
